a73c971397336712268a2945255bb042735eeeeb
Paul Mutinda <paulfelix829@gmail.com>
2021-06-21 10:47:20 -0700

    this is my fourth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
- #include <stdio.h>
- int main (void)
+#include <stdio.h>
+/**
+*main-prints the string in the puts function
+*Description: using the main function
+*This progrm prints "programming is like building a multilingual puzzle
+*Return: 0
+*/
+int main(void)
 {
-  puts ("Programming is like building a multilingual puzzle");
-  return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
