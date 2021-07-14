744802e87d264f0be94564970d7ae632defa5d4b
Amutinda <paulfelix829@gmail.com>
2021-07-14 18:31:22 +0000

    print mem

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,18 @@
 #include "holberton.h"
-
 /**
- * *_memset - fills memory with a constant byte.
- * @s: pointer to put the constant
- * @b: constant
- * @n: max bytes to use
- * Return: s
+ * _memset - check the code for Holberton School students.
+ * @s: parameter for the array
+ * @b: parameter for the values in the array
+ * @n: parameter for the number of times to print the new values
+ * Return: Always 0.
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-  unsigned int i;
-
-  for (i = 0; n > 0; i++, n--)
-    {
-      s[i] = b;
-    }
-
-  return (s);
+unsigned int a = 0;
+while (a < n)
+{
+s[a] = b;
+a++;
+}
+return (s);
 }
